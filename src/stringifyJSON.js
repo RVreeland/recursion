4154157969d22bff7322ffb432da8a628186c349
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-01-13 16:41:35 -0800

    Wrote stringify function. Returns string, but with a few formatting discrepancies.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var petsObj = {
+    Num: 9,
+    Str: "hi",
+    Bool: false,
+    Rose: {
+        hasPets: true,
+        cats: ["Joaquin"],
+        dogs: []
+    },
+    Mom: {
+        hasPets: true,
+        cats: ["William", "Extra Fancy", "Ultimate Precious", "Gray Cat"],
+        dogs: ["Maggie", "Suzie", "Eliza"],
+        donkeys: ["Milo", "Mama", "Mary"]
+    }
 };
+
+var petsArray = [{cat:"William"}, "Extra Fancy", "Ultimate Precious", "Gray Cat"];
+
+var stringifyJSON = function(obj) {
+    var str = "";
+
+    if (typeof obj !== "object") {
+      return typeof obj === "string" ? str += '"' + obj + '",' : str += obj + ",";
+    } else if (Array.isArray(obj)) {
+      str += "["
+      for (var i = 0; i<obj.length; i++) {
+        str += strFunc(obj[i]);
+      }
+      str += "], ";
+    } else {
+      str += "{";
+      for (key in obj) {
+        str += '"' + key + '":';
+        str += strFunc(obj[key]);
+      }
+      str += "},";
+    }
+    return str;
+};
+
