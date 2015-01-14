cbbce9659e9c05e2cdac6ae83d8bb4a1e81a5596
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-01-14 11:35:06 -0800

    stringifyJSON matches JSON.stringify. Passes test. (Added if statement so that functions and undefined in objects will be omittted.)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,25 +3,6 @@
 
 // but you don't so you're going to write it from scratch:
 
-// var petsObj = {
-//     Num: 9,
-//     Str: "hi",
-//     Bool: false,
-//     Rose: {
-//         hasPets: true,
-//         cats: ["Joaquin"],
-//         dogs: {}
-//     },
-//     Mom: {
-//         hasPets: true,
-//         cats: ["William", "Extra Fancy", "Ultimate Precious", "Gray Cat"],
-//         dogs: ["Maggie", "Suzie", "Eliza"],
-//         donkeys: ["Milo", "Mama", "Mary"]
-//     }
-// };
-
-// var petsArray = [{cat:"William"}, "Extra Fancy", "Ultimate Precious", "Gray Cat"];
-
 var stringifyJSON = function(obj) {
     var str = "";
 
@@ -39,48 +20,13 @@ var stringifyJSON = function(obj) {
     } else {
       str += "{";
       for (key in obj) {
-        str += '"' + key + '":';
-        str += stringifyJSON(obj[key]) + ",";
+        if (typeof obj[key] !== "function" && obj[key] !== undefined && typeof obj[key] !== "symbol") {
+          str += '"' + key + '":';
+          str += stringifyJSON(obj[key]) + ",";
+        }
       }
       if (str.charAt(str.length-1) !== "{") { str = str.slice(0, str.length-1) };
       str += "}";
     }
     return str;
 };
-
-
-// var stringifyJSON = function(obj) {
-
-// var stringify = function(obj) {
-//     var str = '';
-
-//     if (typeof obj ==== null) { 
-//       str += "null";
-//     }
-//     else if (typeof obj !== "object") {
-//       typeof obj === "string" ? str += '"' + obj + '"': str += obj;
-//     } else if (Array.isArray(obj)) {
-//       str += "["
-//       for (var i = 0; i<obj.length; i++) {
-//         str += stringify(obj[i]);
-//       }
-//       if (str.charAt(str.length-1) !== "[") { str = str.slice(0, str.length-1) };
-//       str += "]";
-//     } else {
-//       str += "{";
-//       for (key in obj) {
-//         str += '"' + key + '":';
-//         str += stringify(obj[key]);
-//       }
-//       if (str.charAt(str.length-1) !== "{") { str = str.slice(0, str.length-1) };
-//       str += "}";
-//     }
-//     str += ",";
-//     return str;
-// };
-
-// var JSONstring = stringify(obj);
-// var JSONstring = JSONstring.slice(0, JSONstring.length-1);
-// return JSONstring;
-
-// };
