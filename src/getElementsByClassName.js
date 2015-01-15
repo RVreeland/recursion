f3c8b76a01355816d2df4d010d750e705d1511ba
Rebecca Vreeland <rebeccavreeland@gmail.com>
2015-01-14 16:01:24 -0800

    This sort of works. I think it's going through all of the elements and pushing the ones that have the specified class into an array.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className) {
+  var hasClass = [];
+  var bodyNodes = document.body.childNodes;
+
+  var findClasses = function(nodes) {
+    var foundClasses = [];
+    for (var i = 0; i<nodes.length; i++) {
+      var classes = nodes[i].classList;
+      if (classes !== undefined && classes.length !== 0) {
+        for (var j = 0; j<classes.length; j++) {
+            if (classes[j] === className) {
+              alert("found");
+              hasClass.push(nodes[i]);
+              alert(hasClass);
+            }
+        }
+      }
+      if (nodes[i].childNodes.length !== 0) {findClasses(nodes[i].childNodes);}
+    }
+    return hasClass;
+  }
+
+  results = findClasses(bodyNodes);
+  return results;
 };
